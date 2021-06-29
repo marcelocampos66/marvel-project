fa363f1322374633d2e283341a471148b28eb707
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-06-29 19:16:25 -0300

    apaguei logs desnecessarios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,8 @@ import AppContext from '../context/AppContext';
 // import * as S from '../CSS/S.Home';
 
 function Home() {
-<<<<<<< HEAD
-  const result = getCharactersById(498);
-  console.log(result);
-=======
   const { showSearch } = useContext(AppContext);
 
->>>>>>> 92acba403da79652c63e1b71d2a5ca6a0584b8d6
   return (
     <main>
       <Header />
