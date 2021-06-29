60b69b6f8036442d9e5d1dc33baa85ebdc11e582
jhon <jhonatan.arguello@gmail.com>
2021-06-28 21:02:50 -0300

    Revertendo erros do Push anterior

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,11 @@ import Header from '../components/Header.jsx';
 import ListCharacters from '../components/ListCharacters';
 import Search from '../components/Search';
 import AppContext from '../context/AppContext';
-// import * as S from '../CSS/S.Home';
 
 function Home() {
-<<<<<<< HEAD
-  const result = getCharactersById(498);
-  console.log(result);
-=======
+
   const { showSearch } = useContext(AppContext);
 
->>>>>>> 92acba403da79652c63e1b71d2a5ca6a0584b8d6
   return (
     <main>
       <Header />
